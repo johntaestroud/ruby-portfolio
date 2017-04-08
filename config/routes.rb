b07f5e606dc429b9f3d1cedf024146d4e4a16581
Johntae Stroud <johntae.stroud@gmail.com>
2017-04-08 16:05:17 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
+  #bring all of the resource routes except
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
