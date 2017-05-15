1c8fcdb17fadb343b5f37ac006a0f1e9af1726df
Johntae Stroud <johntae.stroud@gmail.com>
2017-05-15 17:58:21 -0400

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,5 +60,18 @@ module ApplicationHelper
     "active" if current_page? path 
   end
 
+  def alerts 
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Stroulbum", sticky: false)
+  end
+
+
 end
 
